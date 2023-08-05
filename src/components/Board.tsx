b5b915faec09958f9aa216f189e6a3b9d83461e2
RimidalU <stankevichuuladimiru@gmail.com>
2023-08-06 00:16:54 +0300

    feat: implement adding symbol to board

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,12 +1,22 @@
 import { getSymbolColor } from "../helpers";
 import { gameBoardField } from "../types";
 
-function Board({ fields }: { fields: gameBoardField[] }) {
+function Board({
+	fields,
+	handleSetStep,
+}: {
+	fields: gameBoardField[];
+	handleSetStep: (index: number) => void;
+}) {
 	return (
 		<div className="grid border-2 grid-cols-[repeat(3,_minmax(50px,_1fr))] grid-rows-[repeat(3,_minmax(50px,_1fr))]">
 			{fields.map((field, index) => {
 				return (
-					<button key={index} className="w-full h-full border-2 text-center self-center">
+					<button
+						key={index}
+						onClick={() => handleSetStep(index)}
+						className="w-full h-full border-2 text-center self-center"
+					>
 						{field && <span className={`font-bold text-3xl ${getSymbolColor(field)}`}>{field}</span>}
 					</button>
 				);
--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,21 +1,33 @@
 import { useState } from "react";
 
-import { gameState } from "../types";
-import { getSymbolColor } from "../helpers";
-import { initialGameState } from "../constants";
 import Board from "./Board";
 
+import { initialGameState, GAME_SYMBOLS } from "../constants";
+import { getSymbolColor } from "../helpers";
+import { gameState } from "../types";
+
 function Game() {
-	const [gameState] = useState<gameState>(initialGameState);
+	const [state, setState] = useState<gameState>(initialGameState);
+
+	const handleSetStep = (index: number) => {
+		const newState = state;
+
+		newState.gameBoard[index] = state.currentStep;
+
+		newState.currentStep === GAME_SYMBOLS.SYMBOL_X
+			? (newState.currentStep = GAME_SYMBOLS.SYMBOL_0)
+			: (newState.currentStep = GAME_SYMBOLS.SYMBOL_X);
+
+		setState({ ...newState });
+	};
+
 	return (
 		<article className=" w-[200px] mx-auto p-3 border-2">
 			<h2 className="text-xl font-bold mb-4">
 				Next step:{""}
-				<span className={`text-3xl ml-3 ${getSymbolColor(gameState.currentStep)}`}>
-					{gameState.currentStep}
-				</span>
+				<span className={`text-3xl ml-3 ${getSymbolColor(state.currentStep)}`}>{state.currentStep}</span>
 			</h2>
-			<Board fields={gameState.gameBoard} />
+			<Board fields={state.gameBoard} handleSetStep={handleSetStep} />
 		</article>
 	);
 }
