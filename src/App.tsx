fde0271a68a5d2dfe33cd604393064840ab96bcf
RimidalU <stankevichuuladimiru@gmail.com>
2023-08-05 19:58:11 +0300

    feat: add tailwind to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-import "./App.css";
-
 function App() {
-	return <h1>hell</h1>;
+	return (
+		<>
+			<header>header</header>
+			<main>
+				<article>
+					<h2 className="text-3xl font-bold underline">Game-info</h2>
+				</article>
+			</main>
+			<footer>footer</footer>
+		</>
+	);
 }
 
 export default App;
