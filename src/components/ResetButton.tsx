82683fe433d9523d6e47ee33b6bdb07bfaf7eb26
RimidalU <stankevichuuladimiru@gmail.com>
2023-08-10 22:44:07 +0300

    refactor: implement clsx in ResetButton component

--- src/components/ResetButton.tsx
+++ src/components/ResetButton.tsx
@@ -1,9 +1,13 @@
+import clsx from "clsx";
 import { GameState } from "../types";
 
 function ResetButton({ state, handleReset }: { state: GameState; handleReset: () => void }) {
 	return (
 		<button
-			className={`border-2 p-1 hover:border-red-500/50 self-center ${state.stopGame && "bg-red-500"}`}
+			className={clsx(
+				"border-2 p-1 hover:border-red-500/50 self-center", 
+				state.stopGame && "bg-red-500"
+			)}
 			onClick={handleReset}
 		>
 			Reset
